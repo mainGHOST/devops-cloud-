b9af3573f8987f7497ec0472bce536ab28f1b8fe
mainGHOST <olawoyinvictor84@gmail.con>
2024-02-15 15:39:18 +0100

    creating s3 bucket

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+aws_region = "eu-north-1"
+bucket_name = "olavicresumee"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  type        = string
+  default     = ""
+  description = "aws region"
+}
+
+variable "bucket_name" {
+  type        = string
+  default     = ""
+  description = "bucket_name"
+}

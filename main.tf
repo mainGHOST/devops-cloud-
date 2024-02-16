fead8697489ca3270b4ac31685635fc895a54ac7
mainGHOST <olawoyinvictor84@gmail.con>
2024-02-16 23:02:04 +0100

    creates and upload content in a s3 bucket

--- main.tf
+++ main.tf
@@ -1,16 +1,26 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = ">= 0.12"
+      source  = "hashicorp/aws"
+      version = ">= 3.0"  
     }
   }
 }
 
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
 
 resource "aws_s3_bucket" "hosting_bucket" {
-    bucket = var.bucket_name
+  bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket_object" "cloude" {
+  bucket = aws_s3_bucket.hosting_bucket.id
+  key    = "cloude/"  # Destination folder in the S3 bucket
+  source = "/root/gitproject/cloude"
+  recursive = true
+}
+
+
+
